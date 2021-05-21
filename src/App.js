8f0ff1a6db2d4d291fef77f44d6e00b396385177
narenm1234 <naren09072020@gmail.com>
2021-05-21 10:31:49 +0530

    Added SideNav Bar Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header/Header';
-import ProgressBar from './components/ProgressBar';
-import VehicleInformation from './components/VehicleInformation';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import MainMenu from './pages/MainMenu';
 import Home from './pages/Home';
 import GroundPending from './pages/GroundPending/GroundPending';
 import { Grounded, Grounded1, LastChance, Passed, Purchased } from './pages/Grounded';
 import '../src/assets/WebFont/ToyotaType-Book.ttf'
 import '../src/sass/main.scss'
+import GroundPassed from './pages/GroundPassed';
+import GroundPurchased from './pages/GroundPurchased';
+import CheckinVehicle from './pages/CheckinVehicle';
 
 function App() {
   // useEffect(() => {
@@ -28,10 +28,10 @@ function App() {
             <Route path='/home' exact component={Home} />
             <Route path='/groundpending' exact component={GroundPending} />
             <Route path='/grounded' exact component={Grounded} />
-            <Route path='/grounded' exact component={Grounded} />
+            <Route path='/checkinvehicle' exact component={CheckinVehicle} />
             <Route path='/grounded/grounded' exact component={Grounded1} />
-            <Route path='/grounded/passed' exact component={Passed} />
-            <Route path='/grounded/purchased' exact component={Purchased} />
+            <Route path='/grounded/passed' exact component={GroundPassed} />
+            <Route path='/grounded/purchased' exact component={GroundPurchased} />
             <Route path='/grounded/lastchance' exact component={LastChance} />
           </Switch>
         </Router>
---
+++ src/pages/CheckinVehicle.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+export default function CheckinVehicle(props) {
+
+    return (
+        <div className='home'>
+            <div className='centerContents'>
+                <h1>CheckinVehicle</h1>
+            </div>
+
+        </div>
+    );
+};
