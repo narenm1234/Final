a0582b92490852d1b1c3cf1ebb8a2bd828c165b8
narenm1234 <naren09072020@gmail.com>
2021-05-24 11:09:42 +0530

    Purchased changes

--- src/pages/GroundPending/purchasedPage.js
+++ src/pages/GroundPending/purchasedPage.js
@@ -9,13 +9,13 @@ import { getPurchasedList } from '../../service/api';
 //let resp = [{ "account_type": "LEASE", "pay_off_amt": 12000, "residual_amt": 11000, "remaining_pmts": 250, "model_year": 19, "brand": "MAZDA", "model": "SD", "ext_color": "Color", "grounding_date": "2021-05-11T00:00:00", "odometer_reading": "9000", "vin": "JM3KFBDM0K1698372" }, { "account_type": "LEASE", "pay_off_amt": 12500, "residual_amt": 9000, "remaining_pmts": 300, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Black", "grounding_date": "2021-05-11T00:00:00", "odometer_reading": "12000", "vin": "JM3KFADM3K1586305" }, { "account_type": "LEASE", "pay_off_amt": 14000, "residual_amt": 11000, "remaining_pmts": 400, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Blue", "grounding_date": "2021-05-07T00:00:00", "odometer_reading": "8000", "vin": "3MZBPBCM4LM125760" }, { "account_type": "LEASE", "pay_off_amt": 15000, "residual_amt": 14000, "remaining_pmts": 500, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Blue", "grounding_date": "2021-05-08T00:00:00", "odometer_reading": "6000", "vin": "JM3KFABM2L0748452" }]
 export default function ListingPage2(props) {
     const [vehicleResponse, setVehicleResponse] = useState([])
-    const [value,setValue] = useState([])
+    const [value, setValue] = useState([])
 
     useEffect(() => {
 
 
         getVehicleDetails()
-    },[value]);
+    }, [value]);
     async function getVehicleDetails() {
         let apiResponse = await getPurchasedList();
         setVehicleResponse(apiResponse.data.data);
@@ -46,11 +46,11 @@ export default function ListingPage2(props) {
                                         </span> */}
 
                                         <span className="textStyle">
-                                        <span className="textBold"> VIN:</span><a className="vin" href="/home"> {vehicle.vin}</a>
+                                            <span className="textBold"> VIN:</span><a className="vin" href="/home"> {vehicle.vin}</a>
                                         </span>
-                                        
+
                                         <span className="textStyle">
-                                        <span className="textBold"> Purchase Date:</span> {moment(vehicle.purchase_date).format('MM/DD/YYYY')}
+                                            <span className="textBold"> Purchase Date:</span> {moment(vehicle.purchase_date).format('MM/DD/YYYY')}
                                         </span>
                                         {/* <span className="textStyle">
                                         <span className="textBold"> Grounding Mileage:</span> {vehicle.odometer_reading} miles
@@ -59,32 +59,35 @@ export default function ListingPage2(props) {
                                         <span className="textBold"> Inspection Mileage:</span> 000,000 miles
                                         </span> */}
                                         <span className="textStyle">
-                                        <span className="textBold"> Purchase Type: </span> {vehicle.purchase_type}
+                                            <span className="textBold"> Purchase Type: </span> {vehicle.purchase_type}
                                         </span>
                                     </List>
 
                                 </div>
                             </Grid>
-                             <Grid item xs={4}>
+                            <Grid item xs={4}>
                                 <div class="Vehicle-Price-Option">
                                     <div class="vehicleMakeModel">
                                         <span>Vehicle Cost </span>
                                     </div>
                                     <List >
                                         <span className="textStyle">
-                                        <span className="textBold"> Payoff Price  </span> <span className="margin__space3">${vehicle.pay_off_amt}</span>
+                                            <span className="textBold"> Payoff Price  </span> <span className="margin__space4">${vehicle.pay_off_amt}</span>
                                         </span>
                                         <span className="textStyle">
-                                        <span className="textBold">  Rem.Payments</span> <span className="margin__space">${ vehicle.remaining_pmts}</span>
+                                            <span className="textBold">  Rem.Payments</span> <span className="margin__space5">${vehicle.remaining_pmts}</span>
                                         </span>
                                         <span className="textStyle">
-                                        <span className="textBold"> Admin Fee</span> <span className="margin__space1">{vehicle.admin_fee}</span>
+                                            <span className="textBold"> Admin Fee</span> <span className="margin__space6">{vehicle.admin_fee}</span>
                                         </span>
-                                       
+                                        <div className="purchasedScreenTotal" />
+                                        <span className="textStyle">
+                                            <span className="textStyleTotalFee"> Total Price</span> <span className="totalFeeSum">$100,000.00</span>
+                                        </span>
                                     </List>
-                                    
+
                                 </div>
-                            </Grid> 
+                            </Grid>
                         </Grid>
 
                     </div>
