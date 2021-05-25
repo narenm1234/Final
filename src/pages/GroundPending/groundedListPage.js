f168ec07dbd0f9e12b2ea04f7af314e2befb3b84
ADITYAH <aditya.harsha@toyota.com>
2021-05-24 22:30:31 -0400

    purchased styling

--- src/pages/GroundPending/groundedListPage.js
+++ src/pages/GroundPending/groundedListPage.js
@@ -38,7 +38,7 @@ export default function ListingPage(props) {
                             <Grid item xs={4}>
                                 <div class="Year-Make-Model-Col">
                                     <div class="vehicleMakeModel">
-                                        <span>{vehicle.brand} {vehicle.model} - {vehicle.text_color} | {vehicle.model_year}</span>
+                                        <span>{vehicle.brand} {vehicle.model}  {vehicle.ext_color}  {vehicle.model_year}</span>
                                     </div>
                                     <List >
 
@@ -110,7 +110,7 @@ export default function ListingPage(props) {
                     </div>
                 )
             }) : (<div className='listingPageCard'>
-                <h1>No  data found</h1>
+                
             </div>)
 
     );
