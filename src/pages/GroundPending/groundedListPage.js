81642f2b627c5534e4bf2c54fce9854a53702c86
narenm1234 <naren09072020@gmail.com>
2021-05-25 20:32:39 +0530

    Responsive Added

--- src/pages/GroundPending/groundedListPage.js
+++ src/pages/GroundPending/groundedListPage.js
@@ -7,23 +7,25 @@ import SwipeableTextMobileStepper from './Carousel';
 import moment from 'moment';
 import { getGroundingList } from '../../service/api';
 import CurrencyFormat from 'react-currency-format';
-
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
 //let resp = [{ "account_type": "LEASE", "pay_off_amt": 12000, "residual_amt": 11000, "remaining_pmts": 250, "model_year": 19, "brand": "MAZDA", "model": "SD", "ext_color": "Color", "grounding_date": "2021-05-11T00:00:00", "odometer_reading": "9000", "vin": "JM3KFBDM0K1698372" }, { "account_type": "LEASE", "pay_off_amt": 12500, "residual_amt": 9000, "remaining_pmts": 300, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Black", "grounding_date": "2021-05-11T00:00:00", "odometer_reading": "12000", "vin": "JM3KFADM3K1586305" }, { "account_type": "LEASE", "pay_off_amt": 14000, "residual_amt": 11000, "remaining_pmts": 400, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Blue", "grounding_date": "2021-05-07T00:00:00", "odometer_reading": "8000", "vin": "3MZBPBCM4LM125760" }, { "account_type": "LEASE", "pay_off_amt": 15000, "residual_amt": 14000, "remaining_pmts": 500, "model_year": 2018, "brand": "MAZDA", "model": "MAZDA3", "ext_color": "Blue", "grounding_date": "2021-05-08T00:00:00", "odometer_reading": "6000", "vin": "JM3KFABM2L0748452" }]
 export default function ListingPage(props) {
     const [vehicleResponse, setVehicleResponse] = useState([])
-    const [value,setValue] = useState([])
+    const [value, setValue] = useState([])
 
     useEffect(() => {
 
 
         getVehicleDetails()
-    },[value]);
+    }, [value]);
     async function getVehicleDetails() {
         let apiResponse = await getGroundingList();
         setVehicleResponse(apiResponse.data.data);
     }
 
-    
+
 
 
     return (
@@ -43,27 +45,27 @@ export default function ListingPage(props) {
                                     <List >
 
                                         <span className="textStyle">
-                                           <span className="textBold"> Exclusivity Period</span>: <span className="textStyleWarning">00:00 Remaining</span>
+                                            <span className="textBold"> Exclusivity Period</span>: <span className="textStyleWarning">00:00 Remaining</span>
                                         </span>
                                         <span className="progressStyle">
                                             <LinearProgress variant="determinate" value={50} color="secondary" />
                                         </span>
 
                                         <span className="textStyle">
-                                        <span className="textBold"> VIN:</span><a className="vin" href="/home"> {vehicle.vin}</a>
+                                            <span className="textBold"> VIN:</span><a className="vin" href="/home"> {vehicle.vin}</a>
                                         </span>
-                                        
+
                                         <span className="textStyle">
-                                        <span className="textBold"> Grounding Date:</span> {moment(vehicle.grounding_date).format('MM/DD/YYYY')}
-                                        </span>
-                                        <span className="textStyle">
-                                        <span className="textBold"> Grounding Mileage:</span>  <CurrencyFormat value={vehicle.odometer_reading} displayType={'text'} thousandSeparator={true} suffix={'  miles'} />
+                                            <span className="textBold"> Grounding Date:</span> {moment(vehicle.grounding_date).format('MM/DD/YYYY')}
                                         </span>
                                         <span className="textStyle">
-                                        <span className="textBold"> Inspection Mileage:</span> 000,000 miles
+                                            <span className="textBold"> Grounding Mileage:</span>  <CurrencyFormat value={vehicle.odometer_reading} displayType={'text'} thousandSeparator={true} suffix={'  miles'} />
                                         </span>
                                         <span className="textStyle">
-                                        <span className="textBold"> Account Type: </span> {vehicle.account_type}
+                                            <span className="textBold"> Inspection Mileage:</span> 000,000 miles
+                                        </span>
+                                        <span className="textStyle">
+                                            <span className="textBold"> Account Type: </span> {vehicle.account_type}
                                         </span>
                                     </List>
 
@@ -75,21 +77,51 @@ export default function ListingPage(props) {
                                         <span>Vehicle Price Option </span>
                                     </div>
                                     <List >
-                                        <span className="textStyle">
-                                        <span className="textBold"> Payoff   </span> 
-                                        <span className="margin__space3"><CurrencyFormat value={vehicle.pay_off_amt} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
-                                        
-                                        </span>
-                                        <span className="textStyle">
-                                       
-                                        <span className="textBold"> Residual + Remaining Payments</span> <span className="margin__space"> <CurrencyFormat value={vehicle.residual_amt + vehicle.remaining_pmts} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
-                                        </span>
-                                        <span className="textStyle">
-                                        <span className="textBold"> Market</span> <span className="margin__space1">$000,000</span>
-                                        </span>
-                                        <span className="textStyle">
-                                        <span className="textBold"> Market + Remaining Payments </span><span className="margin__space2">  <CurrencyFormat value={ vehicle.remaining_pmts} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
-                                        </span>
+                                        <ListItem className="paddingCSS">
+                                            <ListItemText>
+                                                <span className="textStyle">
+                                                    <span className="textBold"> Payoff   </span>
+
+                                                </span>
+                                            </ListItemText>
+                                            <ListItemSecondaryAction>
+                                                <span className="textSize"><CurrencyFormat value={vehicle.pay_off_amt} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+
+                                        <ListItem className="paddingCSS">
+                                            <ListItemText>
+                                                <span className="textStyle">
+                                                    <span className="textBold"> Residual + Remaining Payments   </span>
+
+                                                </span>
+                                            </ListItemText>
+                                            <ListItemSecondaryAction>
+                                                <span className="textSize"><CurrencyFormat value={vehicle.residual_amt + vehicle.remaining_pmts} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                        <ListItem className="paddingCSS">
+                                            <ListItemText>
+                                                <span className="textStyle">
+                                                    <span className="textBold"> Market   </span>
+
+                                                </span>
+                                            </ListItemText>
+                                            <ListItemSecondaryAction>
+                                                <span className="textSize">$000,000</span>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                        <ListItem className="paddingCSS">
+                                            <ListItemText>
+                                                <span className="textStyle">
+                                                    <span className="textBold"> Market + Remaining Payments   </span>
+
+                                                </span>
+                                            </ListItemText>
+                                            <ListItemSecondaryAction>
+                                                <span className="textSize"><CurrencyFormat value={vehicle.remaining_pmts} displayType={'text'} thousandSeparator={true} prefix={'$'} /></span>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
                                     </List>
                                     <div className="NavigatorButtons">
                                         <Button className="PassonVehicleStyle">
