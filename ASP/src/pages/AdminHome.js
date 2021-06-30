7848a91e488c196f04637475faa9c6569eb60087
narenm1234 <naren09072020@gmail.com>
2021-06-30 22:44:52 +0530

    Report and Admin Screen

--- ASP/src/pages/AdminHome.js
+++ ASP/src/pages/AdminHome.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import Search from '../components/Search/Search';
 import AdminVehicleSearch from '../components/AdminVehicleSearch';
 
-
-let styles = {
-  homesearch: {
-    'width': '371px',
-    'height': '32px',
-    'margin': '3px 0 0',
-    'padding': '7px 9px 8px 12px',
-    'border-radius': '4px',
-    'border': 'solid 1px #dddbda',
-    'background-color': '#ffffff'
-
-  },
-}
 const AdminHome = () => {
   return (
     <AdminVehicleSearch />
