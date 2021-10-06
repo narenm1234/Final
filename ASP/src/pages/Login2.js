d0f353f6b48fa17ebfd66d5809adaa9a5ee9643e
ADITYAH <aditya.harsha@toyota.com>
2021-10-05 22:53:33 -0400

    sso

--- ASP/src/pages/Login2.js
+++ ASP/src/pages/Login2.js
@@ -1,33 +1,34 @@
-// import React from 'react'
-// import { getAccessTokenEndpoint, getUserInfoToken } from '../service/api';
+import React,{ useState, useEffect } from 'react'
+import { getAccessTokenEndpoint, getUserInfoToken } from '../service/api';
 
-// let data = getParameterByName("code");
-// function getParameterByName(name, url = window.location.href) {
-//   name = name.replace(/[\[\]]/g, '\\$&');
-//   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-//       results = regex.exec(url);
-//   if (!results) return null;
-//   if (!results[2]) return '';
-//   return decodeURIComponent(results[2].replace(/\+/g, ' '));
-// }
-// useEffect(() => {
-//   getToken();
-//   getUserAccessInfoToken();
-// }, [value]);
-// async function getUserAccessInfoToken(){
-//   let resp = await getUserInfoToken();
-// }
-// async function getToken() {
-//   let apiResponse = await getAccessTokenEndpoint(data);
-//   console.log("--->",apiResponse)
-// }
+let data = getParameterByName("code");
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
-// function Login2() {
-//     return (
-//         <div>
-//            <h1>login2</h1> 
-//         </div>
-//     )
-// }
 
-// export default Login2
+function Login2() {
+  useEffect(() => {
+    getToken();
+    getUserAccessInfoToken();
+  }, []);
+  async function getUserAccessInfoToken(){
+    let resp = await getUserInfoToken();
+  }
+  async function getToken() {
+    let apiResponse = await getAccessTokenEndpoint(data);
+    console.log("--->",apiResponse)
+  }
+    return (
+        <div>
+           <h1>login2</h1> 
+        </div>
+    )
+}
+
+export default Login2
